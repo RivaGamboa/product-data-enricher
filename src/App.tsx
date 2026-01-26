e03e1510341f23042fe2c6b6a9e30e6038cf6f85
RivaGamboa <166232901+RivaGamboa@users.noreply.github.com>
2026-01-26 15:28:57 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Dashboard } from './components/Dashboard';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
